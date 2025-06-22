72c2cb7d9823fc9a81be18fb6342ebcd2e9e8588
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-06-22 17:38:18 -0600

    add tailwindcss
    se agrego tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        hola hola cuidado con la ola... 🌊
-      </p>
+      <header className="flex items-center justify-between p-4 bg-blue-600 text-white">
+        <h1>Universidad Tecnológica de San Miguel de Allende</h1>
+      </header>
     </>
   )
 }
