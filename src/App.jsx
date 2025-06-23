f4c9931a12f589bad21f65d16d046b4674b12e32
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-06-22 21:02:14 -0600

    add fonts
    se agregaron fuentes personalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import Header from "./components/sections/Header"
 function App() {
-
 
   return (
     <>
-      <header className="flex items-center justify-between p-4 bg-blue-600 text-white">
-        <h1>Universidad Tecnológica de San Miguel de Allende</h1>
-      </header>
+      <Header />
     </>
   )
 }
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <header className="contenedor bg-violet-300">
+        <h1>Universidad UTSMA</h1>
+    </header>
+  )
+}
+
+export default Header
