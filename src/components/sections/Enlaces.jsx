395c86fbecb777ac82dfb7363c265423ed137d67
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-06-24 23:59:03 -0600

    iconos
    se agrego un icono personalizado para cada enlace

--- src/components/sections/Enlaces.jsx
+++ src/components/sections/Enlaces.jsx
@@ -1,43 +1,47 @@
-import Icono from "../../assets/images/image.svg";
+import { IconCalendar, IconCap, IconPeople, IconHoja } from "../../helpers/icons";
 
 function Enlaces() {
 
-   const ENLACES = [
-    {
-        title: "Convotorias",
-        icono: Icono,
-        link: "https://www.utsma.edu.mx/convocatorias"
-    },
-    {
-        title: "Oferta Educativa",
-        icono: Icono,   
-        link: "https://www.utsma.edu.mx/oferta-educativa"
-    },
-    {
-        title: "Aspirantes",
-        icono: Icono,   
-        link: "https://www.utsma.edu.mx/aspirantes"
-    },
-    {
-        title: "Calendario Escolar",
-        icono: Icono,   
-        link: "https://www.utsma.edu.mx/calendario-escolar"
-    }
-]
-  return (
-    <section className="w-full py-[100px]">
-        <div className="contenedor">
-            <div className="grid grid-cols-4 gap-4">
-                {ENLACES.map((enlace, index) => (
-                    <a href="{enlace.link}" target="_blank" rel="noopener noreferrer" key={index} className="flex flex-row items-center justify-center gap-3 bg-grisclaroutsma p-6 rounded-lg hover:bg-grisoscuroutsma transition-colors group">
-                        {enlace.icono && <img src={enlace.icono} alt={enlace.title} className="size-8" />   }
-                        <h3 className="font-normal group-hover:text-cyanutsma">{enlace.title}</h3>
-                    </a>
-                ))}
+    const ENLACES = [
+        {
+            title: "Convocatorias",
+            icono: (props) => <IconHoja {...props} />,
+            link: "https://www.utsma.edu.mx/convocatorias",
+        },
+        {
+            title: "Oferta Educativa",
+            icono: (props) => <IconCap {...props} />,
+            link: "https://www.utsma.edu.mx/oferta-educativa",
+        },
+        {
+            title: "Aspirantes",
+            icono: (props) => <IconPeople {...props} />,
+            link: "https://www.utsma.edu.mx/aspirantes",
+        },
+        {
+            title: "Calendario Escolar",
+            icono: (props) => <IconCalendar {...props} />,
+            link: "https://www.utsma.edu.mx/calendario-escolar",
+        }
+    ];
+    return (
+        <section className="w-full py-[100px]">
+            <div className="contenedor">
+                <div className="grid grid-cols-4 gap-4">
+                    {ENLACES.map((enlace, index) => (
+                        <a href="{enlace.link}" target="_blank" rel="noopener noreferrer" key={index} className="flex flex-row items-center justify-center gap-3 bg-grisclaroutsma p-6 rounded-lg hover:bg-grisoscuroutsma transition-colors group">
+                            {enlace.icono({
+                                className: "w-8 h-8 text-gray-500 group-hover:text-cyanutsma transition-colors"
+                            })}
+                            <h3 className="font-normal group-hover:text-cyanutsma">{enlace.title}</h3>
+                        </a>
+                    )
+                    )}
+
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Enlaces
---
+++ src/helpers/icons.jsx
@@ -0,0 +1,109 @@
+export const IconMilk = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    data-name="Layer 1"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path 
+      fill="currentColor"
+      d="M25.89 7.88V2c0-.41-.34-.75-.75-.75H7.39c-.41 0-.75.34-.75.75v5.87l-2.87 3.37s-.03.06-.05.09c-.03.04-.06.09-.08.14-.02.05-.02.1-.03.15 0 .03-.02.07-.02.1V30c0 .41.34.75.75.75h18.28c.09 0 .19-.02.28-.06l5.03-2.05a.76.76 0 0 0 .47-.69V11.73c0-.15-.05-.3-.13-.43l-2.38-3.42Zm-3.64 3.1H5.97L7.65 9h16.08l-1.48 1.98ZM8.14 2.75h16.25V7.5H8.14V2.75Zm-3.05 9.73h16.78v16.77H5.1V12.48Zm21.8 14.97-3.53 1.44V11.98l1.84-2.45 1.69 2.43v15.49Z" />
+  </svg>
+)
+
+
+
+export const IconCalendar = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+      stroke="currentColor"
+      strokeLinejoin="round"
+      d="M3 8.267V19a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V8.267m-18 0V5a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v3.267m-18 0h18"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M7 2v3M17 2v3M18 11h-2M18 17h-2M13 11h-2M13 17h-2M8 11H6M8 17H6M18 14h-2M13 14h-2M8 14H6"
+    />
+  </svg>
+)
+
+
+
+export const IconCap = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    fill="none"
+    {...props}
+  >
+    <path
+      d="M16 4 1 12l15 8 15-8z"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7 15.2V22c0 2.2 4 5 9 5s9-2.8 9-5v-6.8M31 12v13"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+
+
+export const IconHoja = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    fill="none"
+    viewBox="-0.5 0 25 25"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      d="M18.505 22H5.495c-.27 0-.5-.22-.5-.5v-18c0-.27.22-.5.5-.5h13.01c.27 0 .5.22.5.5v18.01c-.01.27-.23.49-.5.49ZM12.995 19h3M7.995 10h7M7.995 12h7M7.995 8h8M7.995 6h8M7.995 14h5.43"
+    />
+  </svg>
+);
+
+
+
+export const IconPeople = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3.5 8a5.5 5.5 0 1 1 8.596 4.547 9.005 9.005 0 0 1 5.9 8.18.75.75 0 0 1-1.5.045 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.499-.044 9.005 9.005 0 0 1 5.9-8.181A5.494 5.494 0 0 1 3.5 8zM9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"
+    />
+    <path
+      fill="currentColor"
+      d="M17.29 8c-.148 0-.292.01-.434.03a.75.75 0 1 1-.212-1.484 4.53 4.53 0 0 1 3.38 8.097 6.69 6.69 0 0 1 3.956 6.107.75.75 0 0 1-1.5 0 5.193 5.193 0 0 0-3.696-4.972l-.534-.16v-1.676l.41-.209A3.03 3.03 0 0 0 17.29 8z"
+    />
+  </svg>
+);
